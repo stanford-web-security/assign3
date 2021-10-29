c8b4813bee8505734b21f5e2a69feb14a16ea60c
Anna Zeng <aszeng@stanford.edu>
2021-10-28 17:12:57 -0700

    Added files in case students want to implement their own server endpoints

---
+++ your-server-code.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+// TODO: Add a server endpoint here if needed for your fingerprinting technique! (optional)
+// e.g.: router.get('/headers', (req, res) => { ... });
+
+module.exports = router
