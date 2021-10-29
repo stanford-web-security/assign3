7bbd116640103ca1dba004e96e5c38b15aa4b824
Timothy Gu <timothygu@stanford.edu>
2021-10-28 17:42:12 -0700

    Add some extra type checking to hash()

--- public/hash.js
+++ public/hash.js
@@ -5,21 +5,36 @@
 function hash (...args) {
   // Convert args to single string
   let concat = '|'
-  for (const elem of args) {
-    // JSON.stringify doesn't do well with Infinity/NaN.
+  for (let elem of args) {
+    // JSON.stringify oddly returns "null" for Infinity/NaN, so special-case.
     if (typeof elem === 'number') {
       concat += String(elem) + '|'
-    } else {
-      try {
-        concat += JSON.stringify(elem) + '|'
-      } catch (e) {
-        console.error('hash: invalid argument', elem)
-        throw new Error('Invalid argument passed in')
-      }
+      continue
+    }
+
+    // Forbid or rewrite some types of objects.
+    if (elem instanceof Promise) {
+      throw new TypeError('Promise object passed to hash()')
+    } else if (elem instanceof Node) {
+      throw new TypeError('DOM Node object passed to hash()')
+    } else if (typeof elem === 'symbol' || elem instanceof Symbol) {
+      throw new TypeError('Symbol passed to hash()')
+    } else if (elem instanceof ArrayBuffer) {
+      // Uint8Array supports JSON.stringify.
+      elem = new Uint8Array(elem)
+    } else if (elem instanceof DataView) {
+      elem = new Uint8Array(elem.buffer, elem.byteOffset, elem.byteLength)
+    }
+
+    try {
+      concat += JSON.stringify(elem) + '|'
+    } catch (e) {
+      console.error('hash: invalid argument', elem)
+      throw new TypeError('Invalid argument passed to hash()')
     }
   }
 
-  // Randomly generated.
+  // Arbitrarily generated.
   const KEY = Uint32Array.of(2029485175, 3096562028, 3968892244, 1289180477)
 
   const hash64 = SipHash.hash_hex(KEY, concat)
